2b2b20493a730eeae9216ddcd39b2166dc340272
rogerspecs <mwambalarogers210@gmail.com>
2021-08-16 11:32:50 +0300

    Did a design on display with css

--- script.js
+++ script.js
@@ -8,28 +8,34 @@ let currentIndex = 0;
 NEXTBTN.addEventListener('click', () => showNextValue());
 PREVIOUSBTN.addEventListener('click', () => showPreviousValue());
 
+const POSiTIONVIEW = document.getElementById('position');
+
+let position = 0;
+let full_position = "";
+let historyTotal = history.length;
+
 function showNextValue(){
-    let historyTotal = history.length;
     let lastIndex = historyTotal  - 1;
+
+    position = currentIndex + 1;
+
     if(currentIndex <= lastIndex){
-        HISTORY.innerHTML = history[currentIndex] + '<button class="remove" value="0"> Delete</button>';
+        full_position = position + "/" + historyTotal
+        HISTORY.innerHTML = history[currentIndex] + '<button class="remove" value="0"> <img src="delete.png" title="Delete icon"></button>';
         currentIndex++;
     }else{
+        full_position = "1/" + historyTotal
+        currentIndex = 0;
         alert("No more history");
     }
-    
-    // let count = 0;
-    // while(count <= lastIndex){
-    //     console.log( history[count]);
-    //     count++;
-    // }
+    POSiTIONVIEW.textContent = full_position
 }
 
 function showPreviousValue(){
     let shownHistory = document.getElementById('history');
     let history_content = shownHistory.textContent;
     let history_value = history_content.split(" ");
-    console.log(history_value)
+    // console.log(history_value)
 
     let actual_history = history_value[0];
     let shownHistoryIndex = history.indexOf(actual_history);
@@ -37,12 +43,21 @@ function showPreviousValue(){
     if(shownHistoryIndex >= 0){
         if(shownHistoryIndex != 0){
             let previousIndex = shownHistoryIndex - 1;
-            HISTORY.innerHTML = history[previousIndex] + '<button class="remove" value="0"> Delete</button>';
+
+            position = previousIndex + 1;
+            full_position = position + "/" + historyTotal
+
+            HISTORY.innerHTML = history[previousIndex] + '<button class="remove" value="0"> <img src="delete.png" title="Delete icon"></button>';
         }
         
     }else{
         // alert("This is the First History Value")
+        full_position = historyTotal + "/" + historyTotal
+
         let previousIndex = history.length - 1;
-        HISTORY.innerHTML = history[previousIndex] + '<button class="remove" value="0"> Delete</button>';
+        HISTORY.innerHTML = history[previousIndex] + '<button class="remove" value="0"> <img src="delete.png" title="Delete icon"></button>';
     }
+
+    POSiTIONVIEW.textContent = full_position
+
 }
